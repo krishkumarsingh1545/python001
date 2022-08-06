2fc4ec77e5b470bf2deb962e75cc631ac7d7a69e
Krish Kumar Singh <105716247+krishkumarsingh1545@users.noreply.github.com>
2022-08-06 21:30:47 +0530

    updated

--- Calculator.py
+++ Calculator.py
@@ -1,16 +1,26 @@
 #----------------------------------------------------------------------------------------------------------------------------
 import math
 #Math module imported
+import time
+#Time module imported
 #----------------------------------------------------------------------------------------------------------------------------
-print('''What would you like to do with this calculator?
-1. Addition
-2. Substraction
-3. Multiplication
-4. Division''')
+print('What would you like to do with this calculator?')
+time.sleep(0.5)
+print('1. Addition')
+time.sleep(0.5)
+print('2. Substraction')
+time.sleep(0.5)
+print('3. Multiplition')
+time.sleep(0.5)
+print('4. Division')
+time.sleep(0.5)
 #----------------------------------------------------------------------------------------------------------------------------
 calc = int(input('Enter Task : ')) 
 x = int(input('Enter 1st number : '))
 y = int(input('Enter 2nd number : '))
+time.sleep(0.5)
+print('Calculating your answer...')
+time.sleep(0.5)
 #----------------------------------------------------------------------------------------------------------------------------
 if  calc ==1:
      print('The sum of %i and %i is %i' % (x, y, x+y))
@@ -26,6 +36,4 @@ else:
 #Ending process command
 end = input('Press Enter to kill process.')
 print('Process Ended')
-#----------------------------------------------------------------------------------------------------------------------------
-import NEW
 #----------------------------------------------------------------------------------------------------------------------------
